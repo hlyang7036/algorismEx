4286ee482cdd66ef55929cd039ade9cbb889b2a5
Hlyang <Hlyang7036@gmail.com>
2019-12-05 02:33:20 +0900

    algorism_11

---
+++ src/algorism_11/Algorism_11.java
@@ -0,0 +1,61 @@
+package algorism_11;
+
+public class Algorism_11 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 1 2 3 4 5 10 9 8 7 6 11 12 13 14 15 20 19 18 17 16 21 22 23 24 25
+		 * 
+		 * 위의 순서대로 숫자를 나열하시오
+		 */
+
+		int inputNum = 5;
+		int arr[] = new int[inputNum + 1];
+		int count = 1;
+		for (int j = 1; j <= inputNum; j++) {
+			if (j % 2 != 0) {
+				for (int i = 1; i <= inputNum; i++) {
+					arr[i] = count;
+					count++;
+				}
+				for (int k = 1; k <= inputNum; k++) {
+					System.out.printf("%4d", arr[k]);
+				}
+
+			} else {
+				for (int i = 1; i <= inputNum; i++) {
+					arr[i] = count;
+					count++;
+				}
+				for (int k = 1; k <= inputNum; k++) {
+					System.out.printf("%4d", arr[inputNum - k + 1]);
+				}
+			}
+			System.out.println();
+		}
+		//============================= 동영상 풀이 ===============================
+		int n = 5;
+		int arrNum[][] = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0) {
+				for (int j = 0; j < n; j++) {
+					arrNum[i][j]= i*n+j+1;
+				}
+			} else {
+				for (int j = n-1; j >=0; j--) {
+					arrNum[i][j]= i*n+n-j;
+				}
+			}
+		}
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%4d", arrNum[i][j]);
+			}
+		System.out.println();
+		}
+	}
+
+}
