b0f251e6c58d534b1ce934bd630726218c698184
Hlyang <Hlyang7036@gmail.com>
2019-12-05 23:11:02 +0900

    algorism_13 풀이

--- src/algorism_13/Algorism_13.java
+++ src/algorism_13/Algorism_13.java
@@ -3,8 +3,48 @@ package algorism_13;
 public class Algorism_13 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		/*
+		 * 입력된 n의 수만큼의 n행 n열의 형태로 보기처럼 숫자를 출력하시오
+		 * 
+		 * n=4;
+		 * 1  2  3  4
+		 * 2  4  6  8
+		 * 3  6  9  12
+		 * 4  8  12 16
+		 * 
+		 */
+		
+		
+//		int n = 4;
+//		int count = 1;
+//		
+//		for (int i = 1; i <= n; i++) {
+//			for (int j = 1; j <= n; j++) {
+//				count  = i*j; 
+//				
+//				System.out.printf("%4d",count);
+//			}
+//			System.out.println();
+//		}
+		
+		int n = 4;
+		int arr[][] = new int [n][n];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = (i+1)*(j+1);
+			}
+		}
+		
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%4d", arr[i][j]);
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
