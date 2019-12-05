d41d5e770dff7601540c8e729b848997992c5be4
Hlyang <Hlyang7036@gmail.com>
2019-12-05 21:49:12 +0900

    algorism_12 풀이

---
+++ src/algorism_12/Algorism_12.java
@@ -0,0 +1,39 @@
+package algorism_12;
+
+public class Algorism_12 {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * 입력된 n의 수만큼 n행 n열의 형태로 보기처럼 숫자를 출력하시오
+		 * 보기
+		 * n=4
+		 * 1  5  9   13
+		 * 2  6  10  14
+		 * 3  7  11  15
+		 * 4  8  12  16
+		 * 
+		 */
+		int n = 4;
+		int arr[][] = new int [n+1][n+1];
+		int count = 1;
+		for (int i = 1; i <= n; i++) {
+			count=i;
+			for (int j = 1; j <= n; j++) {
+				arr[i][j] = count;
+				count+=n;
+			}
+		}
+		
+		
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				System.out.printf("%4d", arr[i][j]);
+			}
+		System.out.println();
+		}
+		
+		
+	}
+
+}
