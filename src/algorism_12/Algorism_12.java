06d1256770e9dde5387bcc405f811b233e9e258e
Hlyang <Hlyang7036@gmail.com>
2019-12-05 22:07:39 +0900

    algorism_12 풀이 추가함

--- src/algorism_12/Algorism_12.java
+++ src/algorism_12/Algorism_12.java
@@ -17,23 +17,41 @@ public class Algorism_12 {
 		int n = 4;
 		int arr[][] = new int [n+1][n+1];
 		int count = 1;
-		for (int i = 1; i <= n; i++) {
-			count=i;
-			for (int j = 1; j <= n; j++) {
-				arr[i][j] = count;
-				count+=n;
+//		for (int i = 1; i <= n; i++) {
+//			count=i;
+//			for (int j = 1; j <= n; j++) {
+//				arr[i][j] = count;
+//				count+=n;
+//			}
+//		}
+
+		
+//		
+//		for (int i = 1; i <= n; i++) {
+//			for (int j = 1; j <= n; j++) {
+//				System.out.printf("%4d", arr[i][j]);
+//			}
+//		System.out.println();
+//		}
+		
+		//=====================================================================
+		
+		int arrNum[][] = new int [n][n];
+		for (int i = 0; i < n; i++) {	// За
+			for (int j = 0; j < n; j++) {	// ї­
+				arrNum[j][i] = count;
+				count++;
 			}
 		}
 		
+
 		
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				System.out.printf("%4d", arr[i][j]);
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.printf("%4d", arrNum[i][j]);
 			}
 		System.out.println();
 		}
-		
-		
 	}
 
 }
---
+++ src/algorism_13/Algorism_13.java
@@ -0,0 +1,10 @@
+package algorism_13;
+
+public class Algorism_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
